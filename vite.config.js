49edf718b180cc6dc20437e5a92902328d83b9cc
Jason Weismueller <60879655+jasonweismueller@users.noreply.github.com>
2025-09-29 21:44:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/social-media-environment/',
+  base: '/social-media-environment-instagram/',
   plugins: [react()],
   build: {
     rollupOptions: {
